703267ea3804f476dde8dfd2278587a4194f0572
josephlocom1-stack <josephlo.com1@gmail.com>
2026-08-25 12:59:13 +1000

    Add timing placeholder for Zelro Reel V6

---
+++ zelro-render/src/timings.ts
@@ -0,0 +1,3 @@
+export type TimedWord = {word: string; startFrame: number; endFrame: number};
+export const WORDS: TimedWord[] = [{word: 'Elon', startFrame: 0, endFrame: 10}];
+export const TOTAL_FRAMES = 570;
