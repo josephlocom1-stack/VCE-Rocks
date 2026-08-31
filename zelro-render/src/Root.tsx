5a7f875f95132abd55a3953e0c140de7253a361d
josephlocom1-stack <josephlo.com1@gmail.com>
2026-08-31 20:01:43 +1000

    Register Elon PayPal premium compositions

--- zelro-render/src/Root.tsx
+++ zelro-render/src/Root.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {ElonFalconReel} from './ElonFalcon';
+import {ElonPayPalReel} from './ElonPayPal';
 import {TOTAL_FRAMES} from './timings';
 
 export const Root: React.FC = () => (
   <>
     <Composition
-      id="ZelroElonFalconPrototype"
-      component={ElonFalconReel}
+      id="ZelroElonPayPalPrototype"
+      component={ElonPayPalReel}
       defaultProps={{prototype: true}}
       durationInFrames={450}
       fps={30}
@@ -15,8 +15,8 @@ export const Root: React.FC = () => (
       height={1920}
     />
     <Composition
-      id="ZelroElonFalcon"
-      component={ElonFalconReel}
+      id="ZelroElonPayPal"
+      component={ElonPayPalReel}
       defaultProps={{prototype: false}}
       durationInFrames={TOTAL_FRAMES}
       fps={30}
